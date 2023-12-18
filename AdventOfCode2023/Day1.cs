5902cfbdcd5d9a0b2afafc9488c62c12f43bfeea
JuliusVanMil <juliusvanmil@outlook.com>
2023-12-18 12:55:00 +0100

    Day 1 part 1

---
+++ AdventOfCode2023/Day1.cs
@@ -0,0 +1,18 @@
+﻿namespace AdventOfCode2023;
+
+public class Day1
+{
+    public static int Part1()
+    {
+        var lines = File.ReadAllLines("Day1Input.txt");
+        var sum = 0;
+        foreach (var line in lines)
+        {
+            var digits = line.Where(char.IsDigit).ToArray();
+            var first = digits.First();
+            var last = digits.Last();
+            sum += int.Parse($"{first}{last}");
+        }
+        return sum;
+    }
+}
