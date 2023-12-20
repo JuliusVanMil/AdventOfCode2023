db95b27596071952c38989d9c4412f63c8ab1972
JuliusVanMil <juliusvanmil@outlook.com>
2023-12-20 15:43:38 +0100

    Clean day 3

--- AdventOfCode2023/Day3.cs
+++ AdventOfCode2023/Day3.cs
@@ -1,103 +1,106 @@
 ﻿using System.Text;
 
-namespace AdventOfCode2023;
-
-public class Day3
+namespace AdventOfCode2023
 {
-    public static string Part1() {
-        var lines = File.ReadAllLines("Day3Input.txt");
-        var adjacentNumbers = new List<int>();
-        for (int i = 0; i < lines.Length; i++)
+    public class Day3
+    {
+        public static string Part1()
         {
-            string current = lines[i];
+            var lines = File.ReadAllLines("Day3Input.txt");
+            var adjacentNumbers = new List<int>();
 
-            // check line above
-            string previous = i > 0 ? lines[i - 1] : "";
-            var previousSymbolIndices = GetSymbolIndices(previous);
-            foreach (var previousSymbolIndex in previousSymbolIndices)
+            for (int i = 0; i < lines.Length; i++)
             {
-                adjacentNumbers.AddRange(TraverseCharacters(current, previousSymbolIndex));
+                string current = lines[i];
+                string previous = i > 0 ? lines[i - 1] : "";
+                string next = i < lines.Length - 1 ? lines[i + 1] : "";
+                adjacentNumbers.AddRange(current.FindNumbersAdjacentToLine(previous));
+                adjacentNumbers.AddRange(current.FindNumbersAdjacentToLine(current));
+                adjacentNumbers.AddRange(current.FindNumbersAdjacentToLine(next));
             }
 
-            // check current line
-            var currentSymbolIndices = GetSymbolIndices(current);
-            foreach (var currentSymbolIndex in currentSymbolIndices)
-            {
-                adjacentNumbers.AddRange(TraverseCharacters(current, currentSymbolIndex));
-            }
-
-            // check line below
-            var next = i < lines.Length - 1 ? lines[i + 1] : "";
-            var nextSymbolIndices = GetSymbolIndices(next);
-            foreach (var nextSymbolIndex in nextSymbolIndices)
-            {
-                adjacentNumbers.AddRange(TraverseCharacters(current, nextSymbolIndex));
-            }
-
+            return adjacentNumbers.Sum().ToString();
         }
-        Console.WriteLine(string.Join(", ", adjacentNumbers));
-        return adjacentNumbers.Sum().ToString();
     }
 
-    private static IEnumerable<int> TraverseCharacters(string current, int currentSymbolIndex)
-    {
-        var numbers = new List<int>();
-        var sb = new StringBuilder();
-        for (int j = currentSymbolIndex - 1; j >= 0; j--)
+    public static class Day3Extensions {
+
+        public static IEnumerable<int> FindNumbersAdjacentToLine(this string current, string line)
         {
-            var currentChar = current[j];
-            if (char.IsDigit(currentChar))
+            var adjacentNumbers = new List<int>();
+            var symbolIndices = GetSymbolIndices(line);
+
+            foreach (var symbolIndex in symbolIndices)
             {
-                sb.Insert(0, currentChar);
+                adjacentNumbers.AddRange(TraverseCharacters(current, symbolIndex));
             }
-            else
+
+            return adjacentNumbers;
+        }
+
+        private static IEnumerable<int> TraverseCharacters(string current, int currentSymbolIndex)
+        {
+            var numbers = new List<int>();
+            var sb = new StringBuilder();
+
+            for (int j = currentSymbolIndex - 1; j >= 0; j--)
             {
-                break;
+                var currentChar = current[j];
+
+                if (char.IsDigit(currentChar))
+                {
+                    sb.Insert(0, currentChar);
+                }
+                else
+                {
+                    break;
+                }
             }
-        }
 
-        var foundNumber = sb.Length > 0;
-        var isCurrentCharacterNumber = char.IsDigit(current[currentSymbolIndex]);
-        var isNextCharacterNumber = currentSymbolIndex < current.Length - 1 && char.IsDigit(current[currentSymbolIndex]);
+            var foundNumber = sb.Length > 0;
+            var isCurrentCharacterNumber = char.IsDigit(current[currentSymbolIndex]);
 
-        if (isCurrentCharacterNumber)
-        {
-            sb.Append(current[currentSymbolIndex]);
-        }
-
-        if (!isNextCharacterNumber && foundNumber)
-        {
-            numbers.Add(int.Parse(sb.ToString()));
-            sb.Clear();
-        }
-
-        for (int j = currentSymbolIndex + 1; j < current.Length; j++)
-        {
-            var currentChar = current[j];
-            if (char.IsDigit(currentChar))
+            if (isCurrentCharacterNumber)
             {
-                sb.Append(currentChar);
+                sb.Append(current[currentSymbolIndex]);
             }
-            else
+
+            if (!isCurrentCharacterNumber && foundNumber)
             {
-                break;
+                numbers.Add(int.Parse(sb.ToString()));
+                sb.Clear();
             }
+
+            for (int j = currentSymbolIndex + 1; j < current.Length; j++)
+            {
+                var currentChar = current[j];
+
+                if (char.IsDigit(currentChar))
+                {
+                    sb.Append(currentChar);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (sb.Length > 0)
+            {
+                numbers.Add(int.Parse(sb.ToString()));
+                sb.Clear();
+            }
+
+            return numbers;
         }
 
-        if (sb.Length > 0)
+        private static List<int> GetSymbolIndices(string line)
         {
-            numbers.Add(int.Parse(sb.ToString()));
-            sb.Clear();
+            return line
+                .Select((character, index) => new { character, index })
+                .Where(x => !char.IsLetterOrDigit(x.character) && x.character != '.')
+                .Select(c => c.index)
+                .ToList();
         }
-
-        return numbers;
-    }
-
-    private static List<int> GetSymbolIndices(string line)
-    {
-        return line
-            .Select((character, index) => new { character, index })
-            .Where(x => !char.IsLetterOrDigit(x.character) && x.character != '.')
-            .Select(c => c.index).ToList();
     }
 }
