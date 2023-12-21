8940ddda0ddf83ee830f6414009ba4927a6c9e77
JuliusVanMil <juliusvanmil@outlook.com>
2023-12-21 15:57:58 +0100

    init day 5

---
+++ AdventOfCode2023/Day5.cs
@@ -0,0 +1,16 @@
+﻿namespace AdventOfCode2023;
+
+public class Day5
+{
+    public static string Part1()
+    {
+        var lines = File.ReadAllLines("Day5Input.txt");
+        return "";
+    }
+
+    public static string Part2()
+    {
+        var lines = File.ReadAllLines("Day5Input.txt");
+        return "";
+    }
+}
