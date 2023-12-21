23c4cc9a2d8f9ee19a77d3aa01d874efb0c3a4ae
JuliusVanMil <juliusvanmil@outlook.com>
2023-12-21 14:29:19 +0100

    day 4 part 1

--- AdventOfCode2023/Day4.cs
+++ AdventOfCode2023/Day4.cs
@@ -4,11 +4,26 @@ public class Day4
 {
     public static string Part1()
     {
-        return "";
+        var lines = File.ReadAllLines("Day4Input.txt");
+        return lines.Select(ParseScratchcard).Select(x => x.Value).Sum().ToString();
     }
 
     public static string Part2()
     {
         return "";
+    }
+
+    public static Scratchcard ParseScratchcard(string line)
+    {
+        var numbers = line.Split(':', StringSplitOptions.TrimEntries)[1].Split('|', StringSplitOptions.TrimEntries);
+        var winningNumbers = numbers[0].Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(int.Parse);
+        var numbersToCheck = numbers[1].Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(int.Parse);
+        return new Scratchcard(winningNumbers, numbersToCheck);
+    }
+
+    public record Scratchcard(IEnumerable<int> WinningNumbers, IEnumerable<int> NumbersToCheck) {
+        public bool IsWinning => WinningNumbers.Any(NumbersToCheck.Contains);
+        public int AmountOfWinningNumbers => WinningNumbers.Count(NumbersToCheck.Contains);
+        public double Value => IsWinning ? Math.Pow(2, AmountOfWinningNumbers - 1) : 0;
     }
 }
