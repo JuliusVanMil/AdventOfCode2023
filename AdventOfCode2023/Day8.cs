40bf17ee233631d1deac8f8b758f84f06ad6162d
JuliusVanMil <juliusvanmil@outlook.com>
2023-12-23 00:34:03 +0100

    day 8 part 1

--- AdventOfCode2023/Day8.cs
+++ AdventOfCode2023/Day8.cs
@@ -22,20 +22,19 @@ public class Day8
         Right = 'R'
     }
 
-    public record Directions(List<Instruction> Instructions, List<Node> Nodes) {
-        public long CountAmountOfInstructionsToReachEnd(string lastNodeName = "ZZZ")
+    public record Directions(List<Instruction> Instructions, Dictionary<string, Node> Nodes)
+    {
+        public int CountAmountOfInstructionsToReachEnd(string startNodeName = "AAA", string lastNodeName = "ZZZ")
         {
-            var currentNode = Nodes.First();
+            var currentNode = Nodes[startNodeName];
             var totalInstructions = Instructions.Count;
-            var instructionCount = 0;
-            long count = 0;
+            int count = 0;
+
             while (currentNode.Name != lastNodeName)
             {
-                var currentInstruction = Instructions[instructionCount];
+                var currentInstruction = Instructions[count % totalInstructions];
                 var nextNode = GetNextNode(currentNode, currentInstruction);
                 currentNode = nextNode;
-
-                instructionCount = (instructionCount + 1) % totalInstructions;
                 count++;
             }
             return count;
@@ -45,29 +44,29 @@ public class Day8
         {
             var nextNodeName = currentInstruction switch
             {
-                Instruction.Left => currentNode.Children.First(),
-                Instruction.Right => currentNode.Children.Last(),
+                Instruction.Left => currentNode.Left,
+                Instruction.Right => currentNode.Right,
                 _ => throw new ArgumentOutOfRangeException(nameof(currentInstruction), currentInstruction, null)
             };
-            return Nodes.First(x => x.Name == nextNodeName);
+            return Nodes[nextNodeName];
         }
 
         public static Directions Parse(string[] lines)
         {
             var instructions = lines[0].Select(x => (Instruction)x).ToList();
-            var nodes = lines[2..].Select(x => Node.Parse(x)).ToList();
+            var nodes = lines[2..].Select(x => Node.Parse(x)).ToDictionary(x => x.Name);
             return new Directions(instructions, nodes);
         }
     }
 
-    public record Node(string Name, List<string> Children)
+    public record Node(string Name, string Left, string Right)
     {
         public static Node Parse(string line)
         {
             var parts = line.Split("=", StringSplitOptions.TrimEntries);
             var name = parts[0].Trim();
             var children = parts[1].Split(",", StringSplitOptions.TrimEntries).Select(x => RemoveParenthesis(x)).ToList();
-            return new Node(name, children);
+            return new Node(name, children[0], children[1]);
         }
 
         private static string RemoveParenthesis(string x)
