ed309adea289528705e1c55f8e88770ed9766700
JuliusVanMil <juliusvanmil@outlook.com>
2023-12-22 10:47:37 +0100

    Cleaned up day 5 part 1

--- AdventOfCode2023/Day5.cs
+++ AdventOfCode2023/Day5.cs
@@ -7,7 +7,7 @@ public class Day5
         var lines = File.ReadAllLines("Day5Input.txt");
         var seeds = lines[0].Split(':')[1].Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).Select(long.Parse).ToList();
         List<Map> maps = ParseMaps(lines);
-        return seeds.Select(s => GetDistance(s, maps)).Min().ToString();
+        return seeds.Select(s => GetDestination(s, maps)).Min().ToString();
     }
 
     public static string Part2()
@@ -18,34 +18,34 @@ public class Day5
 
     private static List<Map> ParseMaps(string[] lines)
     {
-        var ramainingLines = lines.Skip(2).ToArray();
+        var ramainingLines = lines[2..];
 
         var seedToSoilLines = GetLinesAfterMap(ramainingLines);
-        var seedToSoilMap = ParseMap(seedToSoilLines);
+        var seedToSoilMap = Map.Parse(seedToSoilLines);
         ramainingLines = SkipToNextMap(ramainingLines, seedToSoilLines);
 
         var soilToFertilizerLines = GetLinesAfterMap(ramainingLines);
-        var soilToFertilizerMap = ParseMap(soilToFertilizerLines);
+        var soilToFertilizerMap = Map.Parse(soilToFertilizerLines);
         ramainingLines = SkipToNextMap(ramainingLines, soilToFertilizerLines);
 
         var fertilizerToWaterLines = GetLinesAfterMap(ramainingLines);
-        var fertilizerToWaterMap = ParseMap(fertilizerToWaterLines);
+        var fertilizerToWaterMap = Map.Parse(fertilizerToWaterLines);
         ramainingLines = SkipToNextMap(ramainingLines, fertilizerToWaterLines);
 
         var waterToLightLines = GetLinesAfterMap(ramainingLines);
-        var waterToLightMap = ParseMap(waterToLightLines);
+        var waterToLightMap = Map.Parse(waterToLightLines);
         ramainingLines = SkipToNextMap(ramainingLines, waterToLightLines);
 
         var lightToTemperatureLines = GetLinesAfterMap(ramainingLines);
-        var lightToTemperatureMap = ParseMap(lightToTemperatureLines);
+        var lightToTemperatureMap = Map.Parse(lightToTemperatureLines);
         ramainingLines = SkipToNextMap(ramainingLines, lightToTemperatureLines);
 
         var temperatureToHumidityLines = GetLinesAfterMap(ramainingLines);
-        var temperatureToHumidityMap = ParseMap(temperatureToHumidityLines);
+        var temperatureToHumidityMap = Map.Parse(temperatureToHumidityLines);
         ramainingLines = SkipToNextMap(ramainingLines, temperatureToHumidityLines);
 
         var humidityToLocationLines = GetLinesAfterMap(ramainingLines);
-        var humidityToLocationMap = ParseMap(humidityToLocationLines);
+        var humidityToLocationMap = Map.Parse(humidityToLocationLines);
 
         return new List<Map> { seedToSoilMap, soilToFertilizerMap, fertilizerToWaterMap, waterToLightMap, lightToTemperatureMap, temperatureToHumidityMap, humidityToLocationMap };
     }
@@ -60,37 +60,31 @@ public class Day5
         return ramainingLines.Skip(seedToSoilLines.Length + 2).ToArray();
     }
 
-    private static Map ParseMap(string[] lines)
-    {
-        var map = new Map();
-        foreach (var line in lines)
-        {
-            map.RangeMaps.Add(ParseRangeMap(line));
-        }
-        return map;
-    }
-
-    private static RangeMap ParseRangeMap(string line)
-    {
-        var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
-        return new RangeMap(long.Parse(parts[0]), long.Parse(parts[1]), long.Parse(parts[2]));
-    }
-
-    private static long GetDistance(long source, List<Map> maps)
+    private static long GetDestination(long source, List<Map> maps)
     {
         return maps.Aggregate(source, (distance, map) => map.MapToDestination(distance));
     }
 
-    public class Map {
-        public List<RangeMap> RangeMaps { get; set; } = new List<RangeMap>();
+    public record Map(List<RangeMap> RangeMaps) {
         public long MapToDestination(long source) {
             var rangeMap = RangeMaps.FirstOrDefault(r => r.IsInRange(source));
             return rangeMap == null ? source : rangeMap.DestinationStart + (source - rangeMap.SourceStart);
+        }
+
+        public static Map Parse(string[] lines)
+        {
+            var rangemaps = lines.Select(l => RangeMap.Parse(l)).ToList();
+            return new Map(rangemaps);
         }
     }
 
     public record RangeMap(long DestinationStart, long SourceStart, long Length)
     {
         public bool IsInRange(long source) => source >= SourceStart && source <= SourceStart + Length;
+        public static RangeMap Parse(string line)
+        {
+            var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+            return new RangeMap(long.Parse(parts[0]), long.Parse(parts[1]), long.Parse(parts[2]));
+        }
     }
 }
