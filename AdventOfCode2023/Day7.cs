822ab70a45910f5a353d83aac7d7093088e017ef
JuliusVanMil <juliusvanmil@outlook.com>
2023-12-22 20:57:28 +0100

    day 7 init

---
+++ AdventOfCode2023/Day7.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode2023;
+
+public class Day7
+{
+    public static string Part1()
+    {
+        return "Day 7, Part 1";
+    }
+
+    public static string Part2()
+    {
+        return "Day 7, Part 2";
+    }
+}
